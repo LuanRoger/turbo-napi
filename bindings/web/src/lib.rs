207268b45dd1c05d9a32a3995f431ee813861f36
ROG <luanroger.dev@gmail.com>
2026-09-12 09:14:22 -0300

    feat: create proper Node and WASM bindings

--- bindings/web/src/lib.rs
+++ bindings/web/src/lib.rs
@@ -1,7 +1,14 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+#[wasm_bindgen]
 pub fn add(left: f64, right: f64) -> f64 {
+    log("add called");
     turbo_engine::add(left, right)
 }
 
