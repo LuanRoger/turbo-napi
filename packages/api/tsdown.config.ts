dc4a078d5db389b502e7e1ba71bafdda672092a8
ROG <luanroger.dev@gmail.com>
2026-09-12 09:14:05 -0300

    feat: add API package

---
+++ packages/api/tsdown.config.ts
@@ -0,0 +1,17 @@
+import { wasm } from "rolldown-plugin-wasm";
+import { defineConfig } from "tsdown";
+
+export default defineConfig({
+  deps: {
+    neverBundle: ["@turbo-napi/node", "@turbo-napi/web"],
+  },
+  dts: true,
+  entry: {
+    node: "src/node.ts",
+    web: "src/web.ts",
+  },
+  format: "esm",
+  plugins: [wasm()],
+  sourcemap: false,
+  tsconfig: "tsconfig.build.json",
+});
